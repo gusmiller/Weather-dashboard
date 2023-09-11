20712414098472521582b1c2ad6c7ef0beeadf69
Gus Miller <gustavo.miller@miller-hs.com>
2023-09-10 23:13:11 -0400

    Dev002 Search for city / current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,9 +10,81 @@
  * It contains all functions available.
  */
 $(document).ready(function () {
+	const openWeatherAPI = "bbc3d2f0a62f5953d89a98a20be48141";
+	const bingApi = "AiWJmhnxfhdTKeo19xy2NrVRNuGJlN9DiTfsDFV0AbdyHZHjWLv8ru9SloAmYPbk";
+
 	var tasksRegistry = []; // Declare empty array - this will hold the tasks
-	var startingTime = 9; // Scheduler starting time
-	var endingTime = 19; // Scheduler ending time
+	var latitude = ""
+	var longitude = ""
+
+	/**
+	 * This function will connect to the API, request information and process if everything is ok.
+	 * Personal API: 313db44690e1bc1d83bcbf0de3ce1813
+	 * API URL: https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid=313db44690e1bc1d83bcbf0de3ce1813
+	 */
+	function searchInformation() {
+		var selectedCity = $("#searchWeather").val();
+		const geocodingEndpoint = `https://dev.virtualearth.net/REST/v1/Locations?q=${selectedCity}&key=${bingApi}`;
+
+		// Make an HTTP GET fetch request to the API
+		fetch(geocodingEndpoint)
+			.then((response) => response.json())
+			.then((data) => {
+				// Extract the coordinates from the response
+				const firstLocation = data.resourceSets[0].resources[0];
+				latitude = firstLocation.point.coordinates[0];
+				longitude = firstLocation.point.coordinates[1];
+
+				getCoordinates(selectedCity);
+			})
+			.catch((error) => {
+				console.error("Error:", error);
+				return false;
+			});
+	}
+
+	// Create a function to retrieve coordinates
+	/**
+	 * This function will retrieve the coordinates for the selected city using Bing Virtual Earth API for 
+	 * Developers. If the process is correct then It will return the coordinates (latitude/longitude)
+	 * @param {*} city 
+	 */
+	function getCoordinates(city) {
+		const openWeatherURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${openWeatherAPI}`;
+		const forecastEndpoint = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${openWeatherAPI}&units=metric`;
+		//const forecastEndpoint = `https://pro.openweathermap.org/data/2.5/forecast/hourly?lat=${latitude}&lon=${longitude}&appid=${openWeatherAPI}`;
+
+		fetch(forecastEndpoint)
+			.then(response => response.json())
+			.then(data => {
+				// Extract and process the forecast data
+				const cityWeather = data.list[0];
+				const forecasts = data.list
+
+				$("#cityCurrentWeather").text(city + " (" + dayjs().format("DD/MM/YYYY") + ")");
+
+				$("#temperatreHeader").text(cityWeather.main.temp);
+				$("#windHeader").text(cityWeather.wind.speed);
+				$("#humidityHeader").text(cityWeather.main.humidity);
+
+				for (var i=0; i == 5; i++){
+
+				}
+
+				forecasts.forEach(forecast => {
+					const date = forecast.dt_txt;
+					const temperature = forecast.main.temp;
+					const humidity = forecast.main.humidity;
+					const wind = forecast.wind.speed;
+
+					console.log(`Date: ${date}, Temperature: ${temperature}°C, Description: ${weatherDescription}`);
+				});
+			})
+			.catch(error => {
+				console.error('Error:', error);
+			});
+
+	}
 
 	/**
 	 * This function will save the information into the Local Storage. It validates that item doesn't already
@@ -34,7 +106,7 @@ $(document).ready(function () {
 		if (index == null) {
 			// Add new object to the local storage
 			// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
-			tasksRegistry.push(data); 
+			tasksRegistry.push(data);
 		} else {
 			tasksRegistry[index].task = data.task;
 		}
@@ -43,7 +115,7 @@ $(document).ready(function () {
 		// https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
 		// https://www.w3schools.com/Js/js_json_stringify.asp
 		localStorage.setItem(
-			"schedulerTasks",
+			"weatherSearch",
 			JSON.stringify(tasksRegistry)
 		);
 	}
@@ -70,89 +142,19 @@ $(document).ready(function () {
 	 * The function returns a populates -or empty, array that may contain the tasks stored.
 	 */
 	function retrieveData() {
-
-		// Retrieve from local storage the Schedule Tasks and convert into object. We are 
+		// Retrieve from local storage the Schedule Tasks and convert into object. We are
 		// expecting an array
 		// https://www.w3schools.com/Js/js_json_parse.asp
 		// https://developer.mozilla.org/en-US/docs/Web/API/Storage/getItem
-		var tasks = JSON.parse(localStorage.getItem("schedulerTasks"));
+		var autoComplete = JSON.parse(
+			localStorage.getItem("weatherSearch")
+		);
 
-		if (tasks !== null) {
-			// Validate whether we have a single item or an array of objects
-			if (
-				// Prototypes are the mechanism by which JavaScript objects inherit features from one another.
-				// It helps me determine the type of object
-				// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Object_prototypes
-				Object.prototype.toString.call(tasks) !==
-				"[object Array]"
-			) {
-				// Push task into array for further use
-				// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
-				tasksRegistry.push(tasks); // Initialize the tasks array
-			} else {
-				for (i = 0; i <= tasks.length - 1; i++) {
-					// Add task to public array - used in other processes.
-					// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/push
-					tasksRegistry.push(tasks[i]); // Add tasks into array
-
-					// Using JQuery children we access textares and add task retrieve from local storage
-					// https://api.jquery.com/children/
-					$("#" + tasks[i].key)
-						.children("textarea")
-						.val(tasks[i].task);
-				}
-			}
+		if (autoComplete !== null) {
+			$("#searchWeather").autocomplete({
+				source: autoComplete,
+			});
 		}
-	}
-
-	/**
-	 * This function will iterate through the sections in the scheduler and validate current time is being
-	 * properly handled. As time runs the hours change and application needs to refresh its state. This could cause some
-	 * performance issues but it is ok for now.
-	 */
-	function validateCurrent() {
-
-		// This will retrieve the current hour and format it using DayJs library.
-		// https://day.js.org/docs/en/display/format
-		var currentHour = dayjs().format("HH"); 
-		var $parent = $("#timeBlockArea");
-
-		// Iterate through all elements in the timeBlockArea. I had  used a selector like this $(#timeBlockA > Section)
-		// but I did not have the accessibility I was hoping for. This method works fine
-		$parent.children().each(function () {
-			if (this.id != "newTime04") {
-				// Avoid this element -it is hidden.
-
-				var selectTimeBlock = $("#" + this.id); // Build the id in string
-				var text = selectTimeBlock.children().eq(0).text(); // Grab the time from children element
-
-				// This function arses the string argument and returns an integer
-				// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt
-				var hour = parseInt(text.replace(":00", "")); // Remove the minutes format and convert into integer
-
-				// Reset color by removing classes that define the color of the timeBlock
-				// https://www.geeksforgeeks.org/how-to-add-or-remove-class-in-jquery/
-				selectTimeBlock.removeClass(
-					"past present future nonworking"
-				); // Get rid of classes
-
-				// Re-assign the classes based on the time criteria; some hours are non-working hours, past, current
-				// and future hours. Note: requirement does not ask for non-working hours, but I put them for higher
-				// visualization of the day
-				if (+currentHour == hour) {
-					selectTimeBlock.addClass("present");
-					disableItem = "";
-				} else if (
-					hour > +currentHour &&
-					hour <= endingTime - 1
-				) {
-					selectTimeBlock.addClass("future");
-					disableItem = "";
-				} else {
-					selectTimeBlock.addClass("past");
-				}
-			}
-		});
 	}
 
 	/**
@@ -161,64 +163,19 @@ $(document).ready(function () {
 	 * it would not add; as the next row. Leave for later.
 	 */
 	function init() {
-
-		// This will retrieve the current hour and format it using DayJs library.
-		// https://day.js.org/docs/en/display/format
-		var currentHour = dayjs().format("HH"); 
-
-		// Iterate through the hours that would be included in the scheduler. We are using non-working hours
-		// only to demonstrate that we can handle them. This runs automatically when document is ready and it
-		// will start a timer to refresh when the current hour changes.
-		for (var hour = startingTime; hour < endingTime; hour++) {
-
-			// Add leading zeros to the hour. Build time.
-			var formattedHour = addLeadingZero(hour); 
-			var time = formattedHour + ":00";
-			var newTimeFormatted = "newTime" + formattedHour;
-
-			// Work on disabling items and setting up the colors of the time blocks. Note: this will have to be
-			// executed once loaded but then it should trigger every hour.
-			var disableItem = " disabled";
-			var statusTimeColor = "past";
-
-			if (+currentHour == hour) {
-				statusTimeColor = "present";
-				disableItem = "";
-			} else if (hour > +currentHour && hour <= endingTime - 1) {
-				statusTimeColor = "future";
-				disableItem = "";
-			} else {
-				//Uncomment next line to enable data entry on all past timeBlocks. For testing purposes.
-				//disableItem = "";
-			}
-
-			// Build HTML code using string and concatenating variables; slick but not the way I would
-			// like it. Line #62 contains the clone I had in mind, but was not implemented. Kept code for
-			// future implementations.
-			var htmlCode =
-				'<section id="' + newTimeFormatted + '" class="row time-block ' + statusTimeColor + '">' +
-					'<div class="col-2 col-md-1 hour text-center py-3">' + time + "</div>" +
-					'<textarea class="col-8 col-md-10" rows="3" ' + disableItem + "> </textarea>" +
-					'<button type="button" class="btn saveBtn col-2 col-md-1" aria-label="save"' + disableItem + ">" +
-						'<i class="fas fa-save" aria-hidden="true"></i>' + 
-					"</button>" +
-				"</section>";
-
-			// Inserting before the anchor for the order of time
-			// https://developer.mozilla.org/en-US/docs/Web/API/Node/insertBefore
-			$(htmlCode).insertBefore("#anchor"); 
-		}
-
 		// Using DayJs library we display the date on hero section
 		// https://day.js.org/docs/en/display/format
 		$("#currentDay").text(dayjs().format("MMMM DD, YYYY"));
 
-		// Set an interval of 60 seconds to run this function
-		// https://developer.mozilla.org/en-US/docs/Web/API/setInterval
-		setInterval(validateCurrent, 60 * 1000);
+		//var skillNames = ['Toronto','Ottawa','Gatineau'];
+		// Window: localStorage property - data has no expiration time
+		// https://developer.mozilla.org/en-US/docs/Web/API/Storage/setItem
+		// https://www.w3schools.com/Js/js_json_stringify.asp
+		//localStorage.setItem("weatherSearch",JSON.stringify(skillNames));
+
 		retrieveData(); // This will call the function that retrieves data from LocalStorage
 
-		$("button").on("click", saveInformation);
+		$("#searchcity").on("click", searchInformation);
 	}
 
 	/**
